54ef1889273423c76cff61f8af33b23e0cdd62e6
Miranda <Miranda@fanruan.com>
2022-05-08 14:47:59 -0400

    Add routing, links & navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div></div>;
+  // domain:
+  // localhost:3000/__path__
+
+  return (
+    <div>
+      <MainNavigation>
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />} /> {/* default path */}
+          <Route path="/new-meetup" element={<NewMeetupPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </MainNavigation>
+    </div>
+  );
 }
 
 export default App;
