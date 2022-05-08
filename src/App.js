ea21570ac74d0c7fff599653ad02f0508e07efb8
Miranda <Miranda@fanruan.com>
2022-05-08 16:09:18 -0400

    Wrap item cards with Layout

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   // domain:
   // localhost:3000/__path__
 
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} /> {/* default path */}
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
