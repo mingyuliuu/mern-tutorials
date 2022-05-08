364fd1db02ebde4c8dd3d4068f0019c7d20b6411
Miranda <Miranda@fanruan.com>
2022-05-08 11:59:29 -0400

    Add state and event props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <h1>My Todos</h1>
       <Todo text='Learn React'/>
       <Todo text='Master React'/>
-      <Todo text='Explore the full React course'/>
+      <Todo text='Explore the full React course' />
     </div>
   );
 }
