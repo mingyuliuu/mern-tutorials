45cd484a295b26f5d0d0ede5e31dbf8908d5e881
Mingyu Liu <mingyuliu88@gmail.com>
2022-05-10 20:52:25 -0400

    Send POST requests to Firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+      fetch(
+          'https://react-course-8b9c7-default-rtdb.firebaseio.com/meetups.json',
+          {
+              method: 'POST', // default is 'GET'
+              body: JSON.stringify(meetupData),
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          }
+      );
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm></NewMeetupForm>
+      <NewMeetupForm onAddMeetup={addMeetupHandler}></NewMeetupForm>
     </section>
   );
 }
