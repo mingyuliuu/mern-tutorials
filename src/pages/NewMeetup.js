71bc565374b6ab48f4c2329ab0904ecc994c3a4b
Mingyu Liu <mingyuliu88@gmail.com>
2022-05-10 21:30:24 -0400

    Navigate to home page after submitting form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,17 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
-      fetch(
-          'https://react-course-8b9c7-default-rtdb.firebaseio.com/meetups.json',
-          {
-              method: 'POST', // default is 'GET'
-              body: JSON.stringify(meetupData),
-              headers: {
-                  'Content-Type': 'application/json'
-              }
-          }
-      );
+    fetch(
+      "https://react-course-8b9c7-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST", // default is 'GET'
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+        navigate("/", {replace: true});
+    });
   }
 
   return (
